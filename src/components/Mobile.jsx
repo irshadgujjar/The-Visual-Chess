7b5c527c5ef14ca8557ab2671ab34597fa19b951
Irshad <irshadyousuf0075@gmail.com>
2024-05-10 01:28:12 -0700

    updated packages

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -3,5 +3,12 @@ import React from 'react'
 export default function Mobile() {
   return (
     <div>Mobile</div>
+
+
+    // {
+    //     "rewrites":  [
+    //         {"source": "/(.*)", "destination": "/"}
+    //      ]
+    //  }
   )
 }
